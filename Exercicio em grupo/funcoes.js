c27e4b42b273867f02f280249a9f4d6461f872e1
EvertonBoing <evertonboing@outlook.com>
2019-05-27 21:17:14 -0300

    atualiza estilos e atualiza

--- Exercicio em grupo/funcoes.js
+++ Exercicio em grupo/funcoes.js
@@ -2,33 +2,60 @@ const formulario = {}
 var idItem = 1
 
 function chamaBotao() {
+    const codigo = document.getElementById('codigo').value;
     const nome = document.getElementById("nome").value
     const valor = document.getElementById("valor").value
     const URL = document.getElementById("URL").value
     const Descricao = document.getElementById("Descricao").value
+    if(codigo===''){
 
-    const URL2 = 'https://api.conexaonfe.com.br/v1/produtos';
+        const URL2 = 'https://api.conexaonfe.com.br/v1/produtos';
 
-    fetch(URL2, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'bbb',
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                nome: nome,
-                descricao: Descricao,
-                imagem: URL,
-                preco: valor
+        fetch(URL2, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'bbb',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nome: nome,
+                    descricao: Descricao,
+                    imagem: URL,
+                    preco: valor
+                })
             })
-        })
-        .then(async response => {
-            await ListaProdutos()
-        })
-        .catch(error => {
-            console.log(error)
-        });
+            .then(async response => {
+                await ListaProdutos()
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }else{
+        const URL2 = 'https://api.conexaonfe.com.br/v1/produtos/';
 
+        fetch(URL2, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'bbb',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    codigo: codigo,
+                    nome: nome,
+                    descricao: Descricao,
+                    imagem: URL,
+                    preco: valor
+                })
+            })
+            .then(async response => {
+                await ListaProdutos()
+            })
+            .catch(error => {
+                console.log(error)
+            });
+
+
+    }
 
 
     //Limpa formulario
@@ -46,22 +73,23 @@ function additem(jsonItens) {
         let strDiv = `            
                                                 
     	<div id="Contador">                      
-    		<h3>item:${jsonItens[index].codigo}</h3>                     
+    		<h3 id="codigo_${jsonItens[index].codigo}">item:${jsonItens[index].codigo}</h3>                     
     	</div>                                   
                                                 
     	<div id="produtos">                      
-    		<h3>Produto:${jsonItens[index].nome}</h3>    
+    		<h3 id="nome_${jsonItens[index].codigo}" >Produto:${jsonItens[index].nome}</h3>    
     	</div>                                   
                                                 
     	<div id="valor">                         
-    		<h3>Valor:${jsonItens[index].preco}</h3>                    
+    		<h3 id="Valor_${jsonItens[index].codigo}" >Valor:${jsonItens[index].preco}</h3>                    
     	</div>                                   
                                                 
     	<div id="descri">                        
-    		<h3> Descrição:${jsonItens[index].descricao} </h3> 
+    		<h3 id="descri_${jsonItens[index].codigo}" > Descrição:${jsonItens[index].descricao} </h3> 
     	</div>                                   
                                             
-    	<img src="${jsonItens[index].imagem}" alt="produto" class="imgitem"> 
+    	<img id="imagem_${jsonItens[index].codigo}" src="${jsonItens[index].imagem}" alt="produto" class="imgitem"> 
+        <button type="button" class="altera" id='${jsonItens[index].codigo}' onclick="alteraProdutos(this.id)">Altera</button>
 
         <button type="button" class="remove" id='${jsonItens[index].codigo}' onclick="removeElement(this.id)">Remover</button>
         
@@ -73,7 +101,7 @@ function additem(jsonItens) {
         this.idItem = idItemD + 1
         div.innerHTML = strDiv;
         document.getElementById("divItens").appendChild(div);
-
+        //<button type="button" class="altera" id='${jsonItens[index].codigo}' onclick="">altera</button>
     }
 
 }
@@ -102,10 +130,6 @@ function removeElement(id) {
         .catch(error => {
             console.log(error)
         });
-
-
-
-
 
 
 
@@ -141,3 +165,24 @@ function ListaProdutos() {
 
     console.log(`teste`)
 }
+
+function alteraProdutos(id) {
+
+    document.getElementById('codigo').value = document.getElementById("codigo_"+id).innerText.split(":")[1]
+    document.getElementById("nome").value = document.getElementById("nome_"+id).innerText.split(":")[1]
+    document.getElementById("valor").value = document.getElementById("Valor_"+id).innerText.split(":")[1]
+    document.getElementById("URL").value = document.getElementById("imagem_"+id).src
+    document.getElementById("Descricao").value = document.getElementById("descri_"+id).innerText.split(":")[1]
+    // document.getElementById("nome_1").innerText.split(":")[1]
+
+    // document.getElementById("imagem_1").src
+
+
+    //const URL2 = 'https://api.conexaonfe.com.br/v1/produtos';
+
+
+    
+}
+
+
+
